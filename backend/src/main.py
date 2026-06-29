3b1ca6e47036ed172af35f7a97c536ee08fef613
FiaLDI <leon.youtube2017@yandex.ru>
2026-06-29 23:23:05 +0300

    chore: remove old routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -21,8 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(claims_router)
-app.include_router(api_router, prefix="/v1")
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
